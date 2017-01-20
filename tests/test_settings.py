6b38b9234ad559b491575ac250d29f6463b058bb
smallmedialab <accounts@smallmedia.org.uk>
2017-01-20 16:42:46 +0000

    add database to test settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,6 +1,14 @@
 SECRET_KEY = 'fake-key'
+
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "analytics_kits",
     "tests",
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test_database',
+    }
+}
